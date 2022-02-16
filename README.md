7f851a74060158c22a12a0080427af478f08e334
Laura Bnhm <laura.bnhm@gmail.com>
2022-02-16 17:59:17 +0100

    guess the number

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # tp_prog
 
 # Cours lus
+
+LEVEL 1
 01 - Introduction
 02 - Install a compiler
+    put the text cursor on a name + F2 : rename
+    F12 : go to definition
+    ALT + O : switch between .h and .cpp
+    CTRL + F : search for a word in the current file
+    CTRL + SHIFT + F : search for a word in the whole project
+    F7 : build
+    CTRL + F5 : run
 03 - Use an IDE
 04 - Use Git
 05 - Use a debugger
+    CTRL + F5 : run & debug
+    F9 : set a breakpoint
 06 - Use a formatting tool
 07 - Use static analysers
 08 - Naming
 09 - Stack vs Heap
 10 - Consistency in style
+
+# TP faits
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <random>
+using namespace std;
+
+
+int pickRandomNumber(int min, int max);
+int askToUser();
+void greaterOrSmaller();
+
 
 int main()
 {
-    int b;
-    b = 2;
-    std::cout << "Hello World\n";
+    int random_number = pickRandomNumber(0, 100);
+
+    cout << "Guess the number!" << endl;
+    int ask_number = askToUser();
+
+    while(ask_number != random_number)
+    {
+        if(ask_number < random_number)
+        {
+            cout << "Greater" << endl;
+            ask_number = askToUser();
+        }
+            
+        else if(ask_number > random_number)
+        {
+            cout << "Smaller" << endl;
+            ask_number = askToUser();
+        }
+    }
+        
+    cout << "Congrats, you won!" << endl;
+}
+
+
+int pickRandomNumber(int min, int max)
+{
+    static std::default_random_engine  generator{std::random_device{}()};
+    std::uniform_int_distribution<int> distribution{min, max};
+    return distribution(generator);
+}
+
+int askToUser()
+{
+    int ask_number;
+    cout << "Try to guess the number: ";
+    cin >> ask_number;
+    return ask_number;
 }
