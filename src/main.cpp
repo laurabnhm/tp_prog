9f4fce46ea81d62598223ece5a2f1f61e08684ea
Laura Bnhm <laura.bnhm@gmail.com>
2022-02-23 13:59:31 +0100

    guess the number OK

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,9 @@
 #include <random>
 using namespace std;
 
-
-int pickRandomNumber(int min, int max);
-int askToUser();
-void greaterOrSmaller();
-
+#include "play_guess_the_number.h"
 
 int main()
 {
-    int random_number = pickRandomNumber(0, 100);
-
-    cout << "Guess the number!" << endl;
-    int ask_number = askToUser();
-
-    while(ask_number != random_number)
-    {
-        if(ask_number < random_number)
-        {
-            cout << "Greater" << endl;
-            ask_number = askToUser();
-        }
-            
-        else if(ask_number > random_number)
-        {
-            cout << "Smaller" << endl;
-            ask_number = askToUser();
-        }
-    }
-        
-    cout << "Congrats, you won!" << endl;
-}
-
-
-int pickRandomNumber(int min, int max)
-{
-    static std::default_random_engine  generator{std::random_device{}()};
-    std::uniform_int_distribution<int> distribution{min, max};
-    return distribution(generator);
-}
-
-int askToUser()
-{
-    int ask_number;
-    cout << "Try to guess the number: ";
-    cin >> ask_number;
-    return ask_number;
+   play_guess_the_number();
 }
---
+++ src/play_guess_the_number.h
@@ -0,0 +1,3 @@
+int pickRandomNumber(int min, int max);
+int askToUser();
+void play_guess_the_number();
