e0cea8f03381512ff98d249f027d4f5b92486742
Laura Bnhm <laura.bnhm@gmail.com>
2022-03-16 15:48:30 +0100

    adding a menu

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,27 @@
 using namespace std;
 
 #include "common_functions.h"
-//#include "play_guess_the_number.h"
+#include "play_guess_the_number.h"
 #include "play_hangman.h"
 
 int main()
 {
-    //play_guess_the_number();
-    play_hangman();
+    char user_choice;
+
+    cout << "What game do you want to play ?" << endl;
+    cout << "Press 1 to play : Guess the number" << endl;
+    cout << "Press 2 to play : Hangman" << endl;
+    cout << "Press Q to quit" << endl;
+    cin >> user_choice;
+
+    switch(user_choice)
+    {
+        case '1' : play_guess_the_number();
+            break;
+        case '2' : play_hangman();
+            break;
+        case 'Q' : case 'q' : return 0;
+            break;
+        default : cout << "Sorry I don't know that command!" << endl;
+    }
 }
